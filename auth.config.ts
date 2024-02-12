ee0490bf21dd7fd5182f3d4fbdc604abb054b2a3
Yongho Kim <drata313@gmail.com>
2024-02-12 23:54:19 +0900

    Update auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -4,4 +4,21 @@ export const authConfig = {
     pages: {
         signIn: '/login',
     },
+    callbacks: {
+        authroized({ auth, request: { nextUrl } }) {
+            const isLoggedIn = !!auth?.user;
+            const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+            if (isOnDashboard) {
+                if (isLoggedIn) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else if (isLoggedIn) {
+                return Response.redirect(new URL('/dashboard', nextUrl));
+            }
+            return true;
+        },
+    },
+    providers: [],
 };
