f495c4361608e7281f8931f396fb60e869f1ec46
Yongho5580 <70843139+Yongho5580@users.noreply.github.com>
2024-02-13 14:37:58 +0900

    [Updated]: Chapter 16 (All Done)

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -7,6 +7,11 @@ import {
 } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Overview Dashboard'
+}
 
 export default async function Page() {
   return (
